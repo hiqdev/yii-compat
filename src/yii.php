9bc1ee0676b7de2560f5a7aeebae5afa38e34623
Andrii Vasyliev <sol@hiqdev.com>
2019-10-17 17:00:13 +0000

    Refactoring for returning back to Yii2 version 2

--- src/yii.php
+++ src/yii.php
@@ -3,93 +3,111 @@
 namespace hiqdev\yii\compat;
 
 use Yii as Yii2;
-use yii\helpers\Yii as Yii3;
-use yii\profile\Target;
+use yii\di\Instance;
 use yii\mutex\FileMutex;
+use Yiisoft\Factory\Definitions\Reference;
+use Psr\Container\ContainerInterface;
 
-class yii {
+class yii
+{
+    private static $isYii3;
+
     public static function is2()
     {
-        return !class_exists(Yii3::class);
-    }
+        if (self::$isYii3 === null) {
+            self::$isYii3 = class_exists(Reference::class);
+        }
 
-    public static function is2v3()
-    {
-        return class_exists(Target::class);
+        return !self::$isYii3;
     }
 
     public static function is3()
     {
-        return class_exists(Yii3::class);
+        if (self::$isYii3 === null) {
+            self::$isYii3 = class_exists(Reference::class);
+        }
+
+        return self::$isYii3;
     }
 
     public static function get($name)
     {
-        return class_exists(Yii3::class) ? Yii3::get($name) : Yii2::$container->get($name);
+        return self::is3() ? Yii3::get($name) : Yii2::$container->get($name);
     }
 
     public static function getApp()
     {
-        return class_exists(Yii3::class) ? Yii3::getApp() : Yii2::$app;
+        return self::is3() ? Yii3::getApp() : Yii2::$app;
     }
 
     public static function getContainer()
     {
-        return class_exists(Yii3::class) ? Yii3::getContainer() : Yii2::$container;
+        return self::is3() ? Yii3::getContainer() : Yii2::$container;
+    }
+
+    public static function getPsrContainer($container)
+    {
+        return $container instanceof ContainerInterface ? $container : new PsrContainer($container);
+    }
+
+    public static function getPsrLogger($container)
+    {
+        return self::is3() ? $container->get('logger') : new PsrLogger(Yii2::getLogger());
+    }
+
+    public static function getPsrCache($container)
+    {
+        $cache = $container->get('cache');
+        return self::is3() ? $cache : new PsrCache($cache);
     }
 
     public static function getLogger()
     {
-        return class_exists(Yii3::class) ? Yii3::get('logger') : Yii2::getLogger();
+        return self::is3() ? Yii3::getContainer()->get('logger') : Yii2::getLogger();
     }
 
     public static function setLocale($locale)
     {
-        return class_exists(Yii3::class) ? Yii3::getApp()->setLocale($locale) : Yii2::$app->language = $locale;
+        return self::is3() ? Yii3::getApp()->setLocale($locale) : Yii2::$app->language = $locale;
     }
 
     public static function createObject($class, array $params = [])
     {
-        return class_exists(Yii3::class) ? Yii3::createObject($class, $params) : Yii2::createObject($class, $params);
+        return self::is3() ? Yii3::createObject($class, $params) : Yii2::createObject($class, $params);
     }
 
     public static function getAlias($alias, $throwException = true)
     {
-        return class_exists(Yii3::class) ? Yii3::getAlias($alias, $throwException) : Yii2::getAlias($alias, $throwException);
+        return self::is3() ? Yii3::getAlias($alias, $throwException) : Yii2::getAlias($alias, $throwException);
     }
 
     public static function error($message, $category = 'application')
     {
-        return class_exists(Yii3::class) ? Yii3::error($message, $category) : Yii2::error($message, $category);
+        return self::is3() ? Yii3::error($message, $category) : Yii2::error($message, $category);
     }
 
     public static function warning($message, $category = 'application')
     {
-        return class_exists(Yii3::class) ? Yii3::warning($message, $category) : Yii2::warning($message, $category);
-    }
-
-    public static function classKey()
-    {
-        return class_exists(Target::class) || class_exists(Yii3::class) ? '__class' : 'class';
+        return self::is3() ? Yii3::warning($message, $category) : Yii2::warning($message, $category);
     }
 
     public static function referenceTo($id)
     {
-        return class_exists(Yii3::class) ? \yii\di\Reference::to($id) : \yii\di\Instance::of($id);
+        return self::is3() ? Reference::to($id) : Instance::of($id);
     }
 
     public static function classReferenceTo($id)
     {
-        return class_exists(Yii3::class) ? \yii\di\Reference::to($id) : $id;
+        return self::is3() ? Reference::to($id) : $id;
     }
 
     public static function newArrayCache()
     {
-        return class_exists(Yii3::class) ? new \yii\cache\ArrayCache() : new \yii\caching\ArrayCache();
+        return self::is3() ? new \yii\cache\ArrayCache() : new PsrCache(new \yii\caching\ArrayCache());
     }
 
     public static function newFileMutex()
     {
-        return class_exists(Yii3::class) ? new FileMutex(static::getAlias('@runtime/mutex')) : new FileMutex();
+        return self::is3() ? new FileMutex(static::getAlias('@runtime/mutex')) : new FileMutex();
     }
 }
